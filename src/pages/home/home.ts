f848e964089d46d20de888fe0d5f1e296f3b6024
Peter Oliha <poliha2002@yahoo.com>
2017-01-22 23:54:52 +0100

    modifications to add todo list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { ModalController, NavController } from 'ionic-angular';
+import { AddItemPage } from '../add-item-page/add-item-page';
+import {ItemDetailPage } from '../item-detail-page/item-detail-page';
+import { Data } from '../../providers/data';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +11,48 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	public items = [];
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController,
+  	public dataService: Data) {
+
+  	this.dataService.getData().then((todos) => {
+  		if(todos) {
+  			this.items = JSON.parse(todos);
+  		}
+  		
+  	});
 
   }
+
+  ionViewDidLoad(){
+  	console.log("home view loaded");
+  }
+
+  addItem(){
+  	let addModal = this.modalCtrl.create(AddItemPage);
+  	addModal.onDidDismiss((item) => {
+ 
+          if(item){
+            this.saveItem(item);
+          }
+ 
+    });
+ 
+    addModal.present();
+
+  }
+
+  saveItem(item){
+  	this.items.push(item);
+  	this.dataService.saveData(this.items);
+  }
+
+  viewItem(item){
+  	this.navCtrl.push(ItemDetailPage, {
+    	item: item
+  	});
+  }
+
 
 }
